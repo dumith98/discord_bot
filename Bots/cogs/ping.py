f7be6f29b8d829f8597e2fb1494d3aa775d53897
Dumith Bou Habib <dumith98@gmail.com>
2025-04-26 02:50:27 -0300

    renamed bot main file to main.py and successfully made modules with cogs

---
+++ Bots/cogs/ping.py
@@ -0,0 +1,18 @@
+
+import discord
+from discord.ext import commands
+
+class Ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Ping command has loaded successfully!")
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send('pong')
+
+async def setup(bot):
+    await bot.add_cog(Ping(bot))
---
+++ Bots/main.py
@@ -0,0 +1,38 @@
+import os
+import discord
+from discord.app_commands import command
+from dotenv import load_dotenv
+from discord.ext import commands
+import asyncio
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+
+async def Load():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+
+async def main():
+    async with bot:
+        await Load()
+        await bot.start(TOKEN)
+
+# @bot.event
+# async def on_ready():
+#     print(f'{bot.user.name} has connected successfully!')
+#
+# @bot.event
+# async def on_member_join(member):
+#     await member.create_dm()
+#     await member.dm_channel.send(f'Fala {member.name}!, Se esta lendo isso que dizer que deu bom!')
+#
+# @bot.command(name='ping')
+# async def ping(ctx):
+#     await ctx.send('pong')
+
+asyncio.run(main())
