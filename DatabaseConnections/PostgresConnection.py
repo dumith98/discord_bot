85ea7e101995506ce9badd6310202473cc541753
Dumith Bou Habib <dumith98@gmail.com>
2025-05-11 22:09:32 -0300

    changed class initialization to reduce repeated code

--- DatabaseConnections/PostgresConnection.py
+++ DatabaseConnections/PostgresConnection.py
@@ -9,35 +9,34 @@ class PostgresConnection:
         self.user = user
         self.password = password
         self.host = host
+        self.conn = psycopg2.connect(
+            dbname=database, user=user, password=password, host=host
+        )
+        self.cursor = self.conn.cursor()
 
-    def connect(self):
-        dotenv.load_dotenv()
-        try:
-            conn = psycopg2.connect(
-                dbname=os.getenv("database"),
-                user=os.getenv("user"),
-                password=os.getenv("password"),
-                host=os.getenv("host"),
-            )
-            print("\nDatabase successfully connected!\n")
-
-            return conn
-
-        except:
-            print("Connection failed!")
-
-    def getAllNames(self) -> list:
+    def getAllNames(self, tableName: str) -> list:
         """This method returns a list of tuples of each column"""
-        cursor = self.connect().cursor()
-        cursor.execute("select * from  user_table;")
+        try:
+            self.cursor.execute(f"select * from  {tableName};")
 
-        return cursor.fetchall()
+            return self.cursor.fetchall()
+        finally:
+            self.conn.close()
 
+    def insertName(self, tabelName: str, name: str) -> None:
+        try:
+            self.cursor.execute(f"INSERT INTO '{tabelName}' (name) VALUES ('{name}')")
 
+        finally:
+            self.conn.close()
+
+#############   TESTING DB CONNECTION ##############
+import dotenv
+
+dotenv.load_dotenv()
 
 
 postgres = PostgresConnection(
     os.getenv("database"), os.getenv("user"), os.getenv("password"), os.getenv("host")
 )
-postgres.connect()
-print(postgres.getAllNames())
+print(postgres.getAllNames("user_table"))
