d8d9d092c0efc092d54a191e2a2761ea111892f6
Dumith Bou Habib <dumith98@gmail.com>
2025-05-13 22:45:21 -0300

    TypedDict attempt

--- DatabaseConnections/PostgresConnection.py
+++ DatabaseConnections/PostgresConnection.py
@@ -1,16 +1,22 @@
 import psycopg2
 
+from cogs.list_names_postgres_db import DataBaseConfig
+
 
 class PostgresConnection:
-    def __init__(self, database, user, password, host) -> None:
-        self.database = database
-        self.user = user
-        self.password = password
-        self.host = host
-        self.conn = psycopg2.connect(
-            dbname=database, user=user, password=password, host=host
-        )
-        self.cursor = self.conn.cursor()
+    def __init__(self, database_config: DataBaseConfig) -> None:
+        self.database = database_config.get("database")
+        self.user = database_config.get("user")
+        self.password = database_config.get("password")
+        self.host = database_config.get("host")
+        # self.database = database
+        # self.user = user
+        # self.password = password
+        # self.host = host
+        # self.conn = psycopg2.connect(
+        #     dbname=database, user=user, password=password, host=host
+        # )
+        # self.cursor = self.conn.cursor()
 
     def getAllNames(self, tableName: str) -> list:
         """This method returns a list of tuples of each column"""
@@ -21,6 +27,7 @@ class PostgresConnection:
         finally:
             self.conn.close()
 
+    # TODO: implent  C of crud
     def insertName(self, tabelName: str, name: str) -> None:
         try:
             self.cursor.execute(f"INSERT INTO '{tabelName}' (name) VALUES ('{name}')")
--- cogs/list_names_postgres_db.py
+++ cogs/list_names_postgres_db.py
@@ -1,8 +1,18 @@
+from typing import TypedDict
+
+from psycopg2 import DatabaseError
 import discord
 import os
 from DatabaseConnections.PostgresConnection import PostgresConnection
 from discord.ext import commands
 import dotenv
+
+
+class DataBaseConfig(TypedDict):
+    database: str
+    user: str
+    password: str
+    host: str
 
 
 class List(commands.Cog):
@@ -16,13 +26,25 @@ class List(commands.Cog):
     @commands.command()
     async def list(self, ctx):
         dotenv.load_dotenv()
-        postgres = PostgresConnection(
+        # NOTE: Using TypedDict to pass db params
+        postgres = DataBaseConfig(
             os.getenv("database"),
             os.getenv("user"),
             os.getenv("password"),
             os.getenv("host"),
         )
-        await ctx.send(f"{postgres.getAllNames('user_table')}")
+        db_con = PostgresConnection(postgres)
+        #
+        # postgres = PostgresConnection(
+        #     os.getenv("database"),
+        #     os.getenv("user"),
+        #     os.getenv("password"),
+        #     os.getenv("host"),
+        # )
+        template = f"Claro, mestre {ctx.author}.\n Aqui esta a lista de nomes no meu banco de dados: \n"
+        for row in db_con.getAllNames("user_table"):
+            template += f"{row[1]}\n"
+        await ctx.send(template)
 
 
 async def setup(bot):
