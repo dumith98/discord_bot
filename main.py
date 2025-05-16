86c6b074a87fe0911604f38f79fea52d9e3d6e08
Dumith Bou Habib <dumith98@gmail.com>
2025-05-16 18:07:14 -0300

    organized imports with formatter

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import asyncio
 import os
 import discord
-from discord.app_commands import command
-from dotenv import load_dotenv
-from discord.ext import commands
-import asyncio
 import strutlog
+from discord.app_commands import command
+from discord.ext import commands
+from dotenv import load_dotenv
 
 logger = strutlog.get_logger()
 
